509d0d98bc37b433fd3468d4c832c9298b4c7592
CanhHuy <Canhhuy2004@gmail.com>
2025-12-17 15:39:50 +0700

    .

--- user-management/backend/server.js
+++ user-management/backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MSSV = "20225334"; 
+const COLLECTION_NAME = "users";
 // Middleware
 app.use(cors());
 app.use(express.json());
